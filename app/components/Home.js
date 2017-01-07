f7d9eda7a84c4e2f0db166cceb32f4f46086ac27
loolys <pierre.liljekvist@outlook.com>
2017-01-07 11:56:35 +0100

    now logs a submit

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,40 @@
 const React = require('react');
 const styles = require('../styles/main');
 
-const Home = React.createClass({
-  render: function() {
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
     return (
       <div style={styles.alignCenter}>
       <h1>Enter a Location</h1>
-      <form role="search">
+      <form role="search" onSubmit={this.handleSubmit}>
         <div className="form-group" style={styles.homeForm}>
-          <input type="text" className="form-control" placeholder="Search Location" />
+          <input type="text" className="form-control" placeholder="Search Location"
+          value={this.state.value} onChange={this.handleChange} />
         </div>
-        <button type="submit" className="btn btn-default" style={styles.space}>Submit</button>
+        <button type="submit" value="Submit" className="btn btn-default" style={styles.space}>Submit</button>
       </form>
       </div>
     );
   }
-});
+}
+
+
 
 module.exports = Home;
